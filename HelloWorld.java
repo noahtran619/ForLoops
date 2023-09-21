c8c9ee2c7dbf34c64e78f6841e3ccda7b658ce31
Noah Tran <noah.tran@mrjaffesclass.com>
2023-09-21 11:20:32 -0700

    Added Do-While Loops

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,30 +1,33 @@
 public class HelloWorld
 {
-
     int a = 5;
     int b = 2;
     int c = 1;
     int d = -8;
-    
     public void run() {
         int i = 0;
-        while (i < 15) {
-            System.out.println("i is "+ i + " but is less than 15");
+        do {
+            System.out.println("This loop will run as long as i < 15");
             i++;
-        }
+        } 
+        while (i < 15);
+        
         int k = 10;
-        while (k > 5) {
-            System.out.println("k is " + k + " but is greater than 5");
+        do {
+            System.out.println("This loop will run as long as k > 5");
             k -= 1;
-        }
-        while (a < 10 && b<= 20) {
-            System.out.println("a is " + a + " and b is " + b);
-            a = a + 4;
-            b = b + 7;
-        }
-        }
-            public static void main(String[] args)
-    {        
+        } 
+        while (k > 5);
+        
+        do {
+            System.out.println("This loop will run as long as a < 10 and b <= 20");
+            a++;
+            b++;
+        } 
+        while (a < 10 && b<= 20);
+    }
+
+    public static void main(String[] args) {        
         HelloWorld hw = new HelloWorld();
         hw.run();
     }
